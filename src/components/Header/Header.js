5dbfd54c6ca656a833c75120b41d0101bcff99b8
Joppe Kroon <J.Kroon@topdesk.com>
2021-05-25 14:40:09 +0200

    Ex2: Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,6 +23,7 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
@@ -29,9 +32,24 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  
+  height: 4.5rem;
+  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 48px;
 `;
 
-const Nav = styled.nav``;
+const Side = styled.div`
+  flex: 1;
+`;
+
+const Nav = styled.nav`
+  flex: 1;
+  display: flex;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
